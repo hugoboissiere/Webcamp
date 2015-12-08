c0d61b4bab9c900194507ff9ff06ba3161a3c09e
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-08 08:37:49 +0000

    first commit

---
+++ Defi Code Camps - Technologies du Web/ammani_i/admin/php/uploads/upload-file.php
@@ -0,0 +1,18 @@
+<?php
+
+$uploaddir = '../../../preprod/resources/imgs/content/movies/'; 
+$name = $_FILES['uploadfile']['name'];
+$ext = end((explode(".", $name))); # extra () to prevent notice
+//$fileName = "bim"."." . $ext;
+$fileName = $_POST["title"].$_POST["type"]."." . $ext;
+$file = $uploaddir . basename($fileName); 
+ 
+if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], $file)) { 
+  echo "success"; 
+} else {
+	echo "error";
+}
+
+
+
+?>
