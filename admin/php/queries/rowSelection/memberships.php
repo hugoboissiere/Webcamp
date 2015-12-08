4d16afb1e0ae32f1383e4066b2032e5dcc68d44e
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-08 08:37:49 +0000

    first commit

---
+++ admin/php/queries/rowSelection/memberships.php
@@ -0,0 +1,23 @@
+<?php
+$dataBaseHost = 'cl1-sql7';
+$dataBaseName = 'trafalgar6';
+$dataBaseUser = 'trafalgar6';
+$dataBasePassword = 'etna';
+
+
+$id = $_GET['id'];
+
+try {
+    $dbh = new PDO('mysql:host=' . $dataBaseHost . ';dbname=' . $dataBaseName, $dataBaseUser, $dataBasePassword, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    $statement = $dbh->prepare("SELECT * FROM cw_human_resources_memberships WHERE id =".$id."");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+     echo json_encode($results);
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    var_dump($e->getMessage());
+}
