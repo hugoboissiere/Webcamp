246af96dfb8ea163b7a896f7b18be0244277fa5b
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-11 18:18:46 +0000

    test modif head

--- Defi Code Camps - Technologies du Web/ammani_i/admin/movies.php
+++ Defi Code Camps - Technologies du Web/ammani_i/admin/movies.php
@@ -282,25 +282,7 @@ include("php/includes/navigator.php");
                     <th scope="col" data-tablesaw-sortable-col data-tablesaw-priority="3" style="background-color:#232b2d">Images</th>   
                 </tr>
             </thead>
-            <tbody>
-<!--      <!--        <?php
-            while ($result = $moviesCompleteList->fetch()) {
-                ?> -->
-                <tr data-id="<?php echo $result->id; ?>">
-                    <td class="title"><?php echo $result->title; ?></td>
-                    <td><?php echo $result->genre; ?></td>
-                    <td><?php echo $result->releaseDate; ?></td>
-                    <td><?php echo $result->runningTime; ?> min</td> 
-                    <td>
-                        <a href="#picturePoster" class='inline inactiveLink picturePoster' ><img src="resources/imgs/layout/pictureGrey.png"/></a>
-                        <a href="#pictureCover" class='inline inactiveLink pictureCover' ><img src="resources/imgs/layout/pictureGrey.png"/></a>
-                    </td> 
-                </tr>
-                <?php
-            }
-            $moviesCompleteList->closeCursor();
-            ?>    --> 
-            </tbody>
+
         </table>
     </div>
 </main>
