663c65f900242dd06f93a8e656e3482f223e8a2b
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-11 17:34:16 +0000

    test modif head

--- Defi Code Camps - Technologies du Web/ammani_i/admin/php/includes/head.php
+++ Defi Code Camps - Technologies du Web/ammani_i/admin/php/includes/head.php
@@ -1,25 +1,24 @@
 <?php
 session_start();
-if (!isset($_SESSION['auth']) && count($_SESSION['auth']) != 12) {
+if (!isset($_SESSION['auth']) && count($_SESSION['auth']) != 11) {
     header("location:index.php");
     exit;
 }
 
 include("php/queries/count.php");
-include("php/queries/select.php");
+
 // Fichiers CSS/JS/PHP ajoutés
-// $css = $addCSS;
+$css = $addCSS;
 $js = $addJS;
-// $php = $addPHP;
+$php = $addPHP;
 
 
-// if (count($php) > 0 && $php[0] != "") {
-//     for ($i = 0; $i < count($php); $i++) {
-//         include("../php/" . $php[$i] . ".php");
-//     }
-// }
+if (count($php) > 0 && $php[0] != "") {
+    for ($i = 0; $i < count($php); $i++) {
+        include("php/" . $php[$i] . ".php");
+    }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
     <head>
