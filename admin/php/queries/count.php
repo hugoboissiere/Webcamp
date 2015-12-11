57da059e80dcd47867e0e397b2cdfd2815b53ddb
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-11 16:28:52 +0000

    test modif head

--- admin/php/queries/count.php
+++ admin/php/queries/count.php
@@ -35,7 +35,7 @@ else
 	$countHREmployees = $dbh->query("SELECT count(id) as count FROM cw_human_resources_employees WHERE country= '" . $_SESSION['auth']['country'] . "'");
 	$countHREmployees ->setFetchMode(PDO::FETCH_OBJ);
 }
-
+echo "fuewGFUERH";
 // if ($_SESSION['auth']['job'] == 1)
 	$countCinemaSessions = $dbh->query("SELECT count(CCS.id) as count FROM cw_cinema_sessions CCS
 										INNER JOIN cw_cinema_theaters CCT ON CCT.country='".$_SESSION['auth']['country']."'
