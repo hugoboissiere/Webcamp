d39dc8842b17e9f97ef66407dc95b9c3de836f2d
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-11 17:19:18 +0000

    test modif head

--- Defi Code Camps - Technologies du Web/ammani_i/admin/countries.php
+++ Defi Code Camps - Technologies du Web/ammani_i/admin/countries.php
@@ -22,57 +22,7 @@ include("php/includes/navigator.php");
 
 
 <main>
-<!--     <div id="titleAndFilter">
-        <h2><?php echo $pageTitle;?></h2>
-        <ul>
-            <li>
-                <a class='inline' href="#add">Ajouter</a>
-            </li>
-            <li>
-                <a class='inline inactiveLink' href="#modify">Modifier</a>
-            </li>
-            <li>
-                <a class='inline inactiveLink' href="#archive">Archiver</a>
-            </li>
-        </ul>
-    </div>
-    <div style='display:none'>
-        <div class="form-style-8" id="add">
-            <h2>Ajouter</h2>
-            <form method="post" action="" id="addForm" class="formOverflow">
-                <label for="name">Langue : </label>
-                <input type="text" name="name" class="name" placeholder="Espagne">
-                <br/>
-                <label for="abbreviation">Son abréviation : </label>
-                <input type="text" name="abbreviation" class="abbreviation" placeholder="Ex: ES (alpha-2)">
-                <input type="submit" name="insertCountry" value="Enregistrer">
-            </form>
-        </div>
-        <div class="form-style-8" id="modify">
-            <h2>Modifier</h2>
-            <form method="post" action="" id="modificationForm" class="formOverflow">
-                <input type="hidden" class="id" name="id">
-                <label for="name">Langue : </label>
-                <input type="text" name="name" class="name" placeholder="Espagne">
-                <br/>
-                <label for="abbreviation">Son abréviation : </label>
-                <input type="text" name="abbreviation" class="abbreviation" placeholder="Ex: ES (alpha-2)">
-                <input type="submit" name="updateCountry" value="Enregistrer">
-            </form>
-        </div>
-        <div class="form-style-8" id="archive">
-            <h2>Archiver</h2>
-            <form method="post" action="" id="archiveForm" class="formOverflow">
-                <p>Êtes-vous sûr de vouloir archiver ce film ?</p>
-                <br/>
-                <input type="hidden" class="id" name="id">
-                <input type="submit" name="archiveCountry" id="archiveMovieSubmit" value="Oui">
-                <br/>
-                <button type="button" id="cancelArchive" >Non</button>
-                <br/>
-            </form>
-        </div>
-    </div> -->
+
     <div id="table">
         <table class="tablesaw" data-tablesaw-sortable data-tablesaw-mode="stack">
             <thead>
