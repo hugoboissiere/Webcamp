a4163dd6fff3cf447c2fbc585b30cad9b30c395c
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-11 17:47:24 +0000

    test modif head

--- admin/php/queries/select.php
+++ admin/php/queries/select.php
@@ -89,197 +89,197 @@
 $countriesCompleteList = $dbh->query("SELECT * FROM cw_medias_countries  WHERE archive = 'false'");
 $countriesCompleteList->setFetchMode(PDO::FETCH_OBJ);
 
-$countriesListAdd = $dbh->query("SELECT name FROM cw_medias_countries  WHERE archive = 'false'");
-$countriesListAdd->setFetchMode(PDO::FETCH_OBJ);
+// $countriesListAdd = $dbh->query("SELECT name FROM cw_medias_countries  WHERE archive = 'false'");
+// $countriesListAdd->setFetchMode(PDO::FETCH_OBJ);
 
-$countriesListModification = $dbh->query("SELECT name FROM cw_medias_countries  WHERE archive = 'false'");
-$countriesListModification->setFetchMode(PDO::FETCH_OBJ);
+// $countriesListModification = $dbh->query("SELECT name FROM cw_medias_countries  WHERE archive = 'false'");
+// $countriesListModification->setFetchMode(PDO::FETCH_OBJ);
 
-/*
- *      5.  Langages
- */
-
-
-$languageCompleteList =  $dbh->query("SELECT * FROM cw_medias_languages  WHERE archive = 'false'");
-$languageCompleteList -> setFetchMode(PDO::FETCH_OBJ);
+// /*
+//  *      5.  Langages
+//  */
 
 
-/*
- * III. Human Resources
- *      1.  Members
- */
-
-$statusListAdd= $dbh->query("SELECT name FROM cw_human_resources_memberships_status  WHERE archive = 'false'");
-$statusListAdd->setFetchMode(PDO::FETCH_OBJ);
-
-$statusListModification= $dbh->query("SELECT name FROM cw_human_resources_memberships_status  WHERE archive = 'false'");
-$statusListModification->setFetchMode(PDO::FETCH_OBJ);
-
-$activityListAdd= $dbh->query("SELECT name FROM cw_human_resources_memberships_activity  WHERE archive = 'false'");
-$activityListAdd->setFetchMode(PDO::FETCH_OBJ);
-
-$activityListModification= $dbh->query("SELECT name FROM cw_human_resources_memberships_activity  WHERE archive = 'false'");
-$activityListModification->setFetchMode(PDO::FETCH_OBJ);
+// $languageCompleteList =  $dbh->query("SELECT * FROM cw_medias_languages  WHERE archive = 'false'");
+// $languageCompleteList -> setFetchMode(PDO::FETCH_OBJ);
 
 
-/*
- *  Liste complete des inscrits  
- */
+// /*
+//  * III. Human Resources
+//  *      1.  Members
+//  */
 
-if($_SESSION['auth']['job'] == 1)
-{
-	$membershipsCompleteList =  $dbh->query("SELECT * FROM cw_human_resources_memberships  WHERE archive = 'false'");
-	$membershipsCompleteList -> setFetchMode(PDO::FETCH_OBJ);
-}
+// $statusListAdd= $dbh->query("SELECT name FROM cw_human_resources_memberships_status  WHERE archive = 'false'");
+// $statusListAdd->setFetchMode(PDO::FETCH_OBJ);
 
-else
-{
-	$membershipsCompleteList =  $dbh->query("SELECT * FROM cw_human_resources_memberships  WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "'");
-	$membershipsCompleteList -> setFetchMode(PDO::FETCH_OBJ);
-}
+// $statusListModification= $dbh->query("SELECT name FROM cw_human_resources_memberships_status  WHERE archive = 'false'");
+// $statusListModification->setFetchMode(PDO::FETCH_OBJ);
 
-/*
- *          a.  Status
- */
+// $activityListAdd= $dbh->query("SELECT name FROM cw_human_resources_memberships_activity  WHERE archive = 'false'");
+// $activityListAdd->setFetchMode(PDO::FETCH_OBJ);
 
-// NTBD
-
-/*
- *      2.  Employees
- */
-
-/*
- *  Liste complete des inscrits  
- */
-
-if($_SESSION['auth']['job'] == 1)
-{
-	$employeesCompleteList = $dbh->query("SELECT * FROM cw_human_resources_employees  WHERE archive = 'false'");
-	$employeesCompleteList->setFetchMode(PDO::FETCH_OBJ);	
-}
-
-else
-{
-	$employeesCompleteList = $dbh->query("SELECT * FROM cw_human_resources_employees  WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "'");
-	$employeesCompleteList->setFetchMode(PDO::FETCH_OBJ);
-}
-/*
- *  Liste complete des inscrits  
- */
-
-$employeeStatusListAdd = $dbh->query("SELECT * FROM cw_human_resources_employees_status  WHERE archive = 'false'");
-$employeeStatusListAdd->setFetchMode(PDO::FETCH_OBJ);
-
-$employeeStatusListModification = $dbh->query("SELECT * FROM cw_human_resources_employees_status  WHERE archive = 'false'");
-$employeeStatusListModification->setFetchMode(PDO::FETCH_OBJ);
+// $activityListModification= $dbh->query("SELECT name FROM cw_human_resources_memberships_activity  WHERE archive = 'false'");
+// $activityListModification->setFetchMode(PDO::FETCH_OBJ);
 
 
-if($_SESSION['auth']['job'] == 1)
-{
-	$employeeRegionListAdd = $dbh->query("SELECT * FROM region");
-	$employeeRegionListAdd->setFetchMode(PDO::FETCH_OBJ);
-}
+// /*
+//  *  Liste complete des inscrits  
+//  */
 
-else
-{
-	$employeeRegionListAdd = $dbh->query("SELECT * FROM region WHERE id_region = '" . $_SESSION['auth']['job'] . "'");
-	$employeeRegionListAdd->setFetchMode(PDO::FETCH_OBJ);
-}
+// if($_SESSION['auth']['job'] == 1)
+// {
+// 	$membershipsCompleteList =  $dbh->query("SELECT * FROM cw_human_resources_memberships  WHERE archive = 'false'");
+// 	$membershipsCompleteList -> setFetchMode(PDO::FETCH_OBJ);
+// }
 
-$employeeRegionListModification = $dbh->query("SELECT * FROM region");
-$employeeRegionListModification->setFetchMode(PDO::FETCH_OBJ);
+// else
+// {
+// 	$membershipsCompleteList =  $dbh->query("SELECT * FROM cw_human_resources_memberships  WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "'");
+// 	$membershipsCompleteList -> setFetchMode(PDO::FETCH_OBJ);
+// }
 
-/*
- *          a.  Status
- */
+// /*
+//  *          a.  Status
+//  */
 
-/*
- *          b.  Jobs
- */
+// // NTBD
 
-/*
- *  * IV.  The Theaters
- *      1.  Sessions (movies' sessions)
- */
+// /*
+//  *      2.  Employees
+//  */
 
-if ($_SESSION['auth']['job'] == 1)
-{
-	$sessionsCompleteList = $dbh->query("SELECT s.id, m.title, m.runningTime, s.date, t.name as theater
-                                FROM cw_medias_movies m, cw_cinema_sessions s, cw_cinema_theaters t
-                                WHERE t.id = s.idTheater AND s.idMovie = m.id AND s.archive = 'false'");        
-	$sessionsCompleteList->setFetchMode(PDO::FETCH_OBJ);
+// /*
+//  *  Liste complete des inscrits  
+//  */
 
-	$theatersCompleteList = $dbh->query("SELECT name, id FROM cw_cinema_theaters WHERE archive = 'false'");      
-	$theatersCompleteList->setFetchMode(PDO::FETCH_OBJ);
+// if($_SESSION['auth']['job'] == 1)
+// {
+// 	$employeesCompleteList = $dbh->query("SELECT * FROM cw_human_resources_employees  WHERE archive = 'false'");
+// 	$employeesCompleteList->setFetchMode(PDO::FETCH_OBJ);	
+// }
 
+// else
+// {
+// 	$employeesCompleteList = $dbh->query("SELECT * FROM cw_human_resources_employees  WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "'");
+// 	$employeesCompleteList->setFetchMode(PDO::FETCH_OBJ);
+// }
+// /*
+//  *  Liste complete des inscrits  
+//  */
 
-	$languagesCompleteList = $dbh->query("SELECT name, id FROM cw_medias_languages WHERE archive = 'false'");      
-	$languagesCompleteList->setFetchMode(PDO::FETCH_OBJ);
-}
+// $employeeStatusListAdd = $dbh->query("SELECT * FROM cw_human_resources_employees_status  WHERE archive = 'false'");
+// $employeeStatusListAdd->setFetchMode(PDO::FETCH_OBJ);
 
-else
-{
-	$sessionsCompleteList = $dbh->query("SELECT s.id, m.title, m.runningTime, s.date, t.name as theater
-                                FROM cw_medias_movies m, cw_cinema_sessions s, cw_cinema_theaters t
-                                WHERE t.id = s.idTheater AND s.idMovie = m.id AND s.archive = 'false' AND t.region = '" . $_SESSION['auth']['region'] . "'");        
-	$sessionsCompleteList->setFetchMode(PDO::FETCH_OBJ);
-
-	$theatersCompleteList = $dbh->query("SELECT name, id FROM cw_cinema_theaters WHERE archive = 'false'");      
-	$theatersCompleteList->setFetchMode(PDO::FETCH_OBJ);
+// $employeeStatusListModification = $dbh->query("SELECT * FROM cw_human_resources_employees_status  WHERE archive = 'false'");
+// $employeeStatusListModification->setFetchMode(PDO::FETCH_OBJ);
 
 
-	$languagesCompleteList = $dbh->query("SELECT name, id FROM cw_medias_languages WHERE archive = 'false'");      
-	$languagesCompleteList->setFetchMode(PDO::FETCH_OBJ);
-}
+// if($_SESSION['auth']['job'] == 1)
+// {
+// 	$employeeRegionListAdd = $dbh->query("SELECT * FROM region");
+// 	$employeeRegionListAdd->setFetchMode(PDO::FETCH_OBJ);
+// }
 
-/*
- *      2.  Theaters (locations)
- */
-if($_SESSION['auth']['job'] == 1)
-{
-	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false'");
-	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
-}
+// else
+// {
+// 	$employeeRegionListAdd = $dbh->query("SELECT * FROM region WHERE id_region = '" . $_SESSION['auth']['job'] . "'");
+// 	$employeeRegionListAdd->setFetchMode(PDO::FETCH_OBJ);
+// }
 
-else
-{
-	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "' ");
-	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
-}
+// $employeeRegionListModification = $dbh->query("SELECT * FROM region");
+// $employeeRegionListModification->setFetchMode(PDO::FETCH_OBJ);
+
+// /*
+//  *          a.  Status
+//  */
+
+// /*
+//  *          b.  Jobs
+//  */
+
+// /*
+//  *  * IV.  The Theaters
+//  *      1.  Sessions (movies' sessions)
+//  */
+
+// if ($_SESSION['auth']['job'] == 1)
+// {
+// 	$sessionsCompleteList = $dbh->query("SELECT s.id, m.title, m.runningTime, s.date, t.name as theater
+//                                 FROM cw_medias_movies m, cw_cinema_sessions s, cw_cinema_theaters t
+//                                 WHERE t.id = s.idTheater AND s.idMovie = m.id AND s.archive = 'false'");        
+// 	$sessionsCompleteList->setFetchMode(PDO::FETCH_OBJ);
+
+// 	$theatersCompleteList = $dbh->query("SELECT name, id FROM cw_cinema_theaters WHERE archive = 'false'");      
+// 	$theatersCompleteList->setFetchMode(PDO::FETCH_OBJ);
 
 
+// 	$languagesCompleteList = $dbh->query("SELECT name, id FROM cw_medias_languages WHERE archive = 'false'");      
+// 	$languagesCompleteList->setFetchMode(PDO::FETCH_OBJ);
+// }
+
+// else
+// {
+// 	$sessionsCompleteList = $dbh->query("SELECT s.id, m.title, m.runningTime, s.date, t.name as theater
+//                                 FROM cw_medias_movies m, cw_cinema_sessions s, cw_cinema_theaters t
+//                                 WHERE t.id = s.idTheater AND s.idMovie = m.id AND s.archive = 'false' AND t.region = '" . $_SESSION['auth']['region'] . "'");        
+// 	$sessionsCompleteList->setFetchMode(PDO::FETCH_OBJ);
+
+// 	$theatersCompleteList = $dbh->query("SELECT name, id FROM cw_cinema_theaters WHERE archive = 'false'");      
+// 	$theatersCompleteList->setFetchMode(PDO::FETCH_OBJ);
+
+
+// 	$languagesCompleteList = $dbh->query("SELECT name, id FROM cw_medias_languages WHERE archive = 'false'");      
+// 	$languagesCompleteList->setFetchMode(PDO::FETCH_OBJ);
+// }
+
+// /*
+//  *      2.  Theaters (locations)
+//  */
+// if($_SESSION['auth']['job'] == 1)
+// {
+// 	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false'");
+// 	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
+// }
+
+// else
+// {
+// 	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "' ");
+// 	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
+// }
 
 
 
 
-/*
- * V.   Tools
- *      1.  Cash Register
- */
 
-// NTBD
 
- /* 
- *      2.  Statistics
- */
+// /*
+//  * V.   Tools
+//  *      1.  Cash Register
+//  */
 
-// NTBD
+// // NTBD
 
- /* 
- *      3.  Newsletter
- */
+//  /* 
+//  *      2.  Statistics
+//  */
 
-// NTBD
+// // NTBD
 
- /* 
- *      4.  History
- */
+//  /* 
+//  *      3.  Newsletter
+//  */
 
-// NTBD
+// // NTBD
 
- /*
- *      5.  Cloud
- */ 
+//  /* 
+//  *      4.  History
+//  */
 
-// NTBD
+// // NTBD
+
+//  /*
+//  *      5.  Cloud
+//  */ 
+
+// // NTBD
 
