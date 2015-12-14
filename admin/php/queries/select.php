c2bf3a82f9cd34e9f69dd7b1388652fd95eba95d
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 13:32:15 +0000

    ca marche pas

--- admin/php/queries/select.php
+++ admin/php/queries/select.php
@@ -223,12 +223,11 @@ if($_SESSION['auth']['job'] == 1)
 	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false'");
 	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
 }
-
-// else
-// {
-// 	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false' AND region= '" . $_SESSION['auth']['region'] . "' ");
-// 	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
-// }
+else
+{
+	$theaterCompleteList =  $dbh->query("SELECT * FROM cw_cinema_theaters WHERE archive = 'false' AND country= '" . $_SESSION['auth']['country'] . "' ");
+	$theaterCompleteList -> setFetchMode(PDO::FETCH_OBJ);
+}
 
 
 
