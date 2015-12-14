3cf42a8d610fbf981096d80f63416054d1cb2333
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 18:26:07 +0000

    oui

--- public/reservation.php
+++ public/reservation.php
@@ -19,117 +19,114 @@ $sessionsCompleteList->setFetchMode(PDO::FETCH_OBJ);
 
 
 	
+$id = $_GET['id'];
+
+$movieDetail = $dbh->query("SELECT * FROM cw_medias_movies WHERE id = '" . $id . "'");
+$movieDetail->setFetchMode(PDO::FETCH_OBJ);
 
 
+ while ($result = $movieDetail->fetch()) {
 
 
+     
+     
+     
+       $filenamePoster = 'resources/imgs/content/movies/'.$result->id.'Poster.jpg';
+
+if (file_exists($filenamePoster)) {
+    $img = $filenamePoster;
+} else {
+   $rand =  rand(1,2);
+   $img = 'resources/imgs/content/movies/defaultPoster'.$rand.'.jpg';
+}  
 
 
-// }
-// $sessionsCompleteList->closeCursor();
-// $id = $;
+  $filenameCover = 'resources/imgs/content/movies/'.$result->id.'Cover.jpg';
 
-	$movieDetail = $dbh->query("SELECT * FROM cw_medias_movies");
-	$movieDetail->setFetchMode(PDO::FETCH_OBJ);
+if (file_exists($filenameCover)) {
+    $cover = $filenameCover;
+} else {
+   $rand =  rand(1,2);
+   $cover = 'resources/imgs/content/movies/defaultCover'.$rand.'.jpg';
+}  
+     
 
+?>
 
-	while ($result = $movieDetail->fetch()) {
+<script>
+    $(document).ready(function(){
+        $(".trailer").colorbox({iframe:true, innerWidth:500, innerHeight:409});
 
-		$filenamePoster = 'resources/imgs/content/movies/'.$result->id.'Poster.jpg';
+    });
+</script>
+<div id="cover">
+      <span></span>
+      <img src="<?php echo $cover; ?>" alt="">
 
-		if (file_exists($filenamePoster)) {
-			$img = $filenamePoster;
-		} else {
-			$rand =  rand(1,2);
-			$img = 'resources/imgs/content/movies/defaultPoster'.$rand.'.jpg';
-		}  
+      <h2>
+        <?php echo $result->title; ?>
+        <span><?php echo $result->titleOriginal; ?></span>
 
+      </h2>
+      <?php if ($result->type != ""){ ?>
+      <a class="trailer" href="<?php echo $result->type; ?>">Bande-Annonce</a>
+      <?php } ?>
+  </div>
+  <div id="informations" style="margin-top:300px;">
+    <div id="picture">
+        <img src="<?php echo $img; ?>" alt="">
+    </div>
+    <div id="text">
+        <div id="title">
+            <ul>
+                <li>
+                    <span class="dataTitle">Date de sortie</span>
+                    <span class="dataContent"><?php echo $result->releaseDate; ?></span>
+                </li>
+                <li>
+                    <span class="dataTitle">DurÃ©e</span>
+                    <span class="dataContent"><?php echo $result->runningTime; ?> Minutes</span>
+                </li>
+                <li>
+                    <span class="dataTitle">Genre</span>
+                    <span class="dataContent"><?php echo $result->genre;?></span>
+                </li>
+                <li>
+                    <span class="dataTitle">RÃ©alisateur</span>
+                    <span class="dataContent"><?php echo $result->realisator; ?></span>
+                </li>
+                <li>
+                    <span class="dataTitle">Acteurs</span>
+                    <span class="dataContent"><?php echo $result->actors; ?></span>
+                </li>
+                <li>
+                    <span class="dataTitle">Production</span>
+                    <span class="dataContent"><?php echo $result->production; ?></span>
+                </li>
+                <li>
+                    <span class="dataTitle">Pays</span>
+                    <span class="dataContent"><?php echo $result->country ?></span>
+                </li>
+                <li>
+                    <span class="dataTitle">Langue</span>
+                    <span class="dataContent"><?php echo $result->language; ?></span>
+                </li>
+                <li>
+                    <span class="dataPlot">Description</span>
+                    <span class="dataContent"><?php echo $result->plot; ?></span>
+            </ul>
+        </div>
+    </div>
+<?php
+    
+            }
 
-		$filenameCover = 'resources/imgs/content/movies/'.$result->id.'Cover.jpg';
+            $movieDetail->closeCursor();
 
-		if (file_exists($filenameCover)) {
-			$cover = $filenameCover;
-		} else {
-			$rand =  rand(1,2);
-			$cover = 'resources/imgs/content/movies/defaultCover'.$rand.'.jpg';
-		}  
+            ?> 
+<?php
+include("php/includes/scripts.php");
+?>
 
-
-		?>
-
-		<script>
-		$(document).ready(function(){
-			$(".trailer").colorbox({iframe:true, innerWidth:500, innerHeight:409});
-
-		});
-		</script>
-		<div id="cover">
-			<span></span>
-			<img src="<?php echo $cover; ?>" alt="">
-
-			<h2>
-				<?php echo $result->title; ?>
-				<span><?php echo $result->titleOriginal; ?></span>
-
-			</h2>
-			<?php if ($result->type != ""){ ?>
-			<a class="trailer" href="<?php echo $result->type; ?>">Bande-Annonce</a>
-			<?php } ?>
-		</div>
-		<div id="informations" style="margin-top:300px;">
-			<div id="picture">
-				<img src="<?php echo $img; ?>" alt="">
-			</div>
-			<div id="text">
-				<div id="title">
-					<ul>
-						<li>
-							<span class="dataTitle">Date de sortie</span>
-							<span class="dataContent"><?php echo $result->releaseDate; ?></span>
-						</li>
-						<li>
-							<span class="dataTitle">DurÃ©e</span>
-							<span class="dataContent"><?php echo $result->runningTime; ?> Minutes</span>
-						</li>
-						<li>
-							<span class="dataTitle">Genre</span>
-							<span class="dataContent"><?php echo $result->genre;?></span>
-						</li>
-						<li>
-							<span class="dataTitle">RÃ©alisateur</span>
-							<span class="dataContent"><?php echo $result->realisator; ?></span>
-						</li>
-						<li>
-							<span class="dataTitle">Acteurs</span>
-							<span class="dataContent"><?php echo $result->actors; ?></span>
-						</li>
-						<li>
-							<span class="dataTitle">Production</span>
-							<span class="dataContent"><?php echo $result->production; ?></span>
-						</li>
-						<li>
-							<span class="dataTitle">Pays</span>
-							<span class="dataContent"><?php echo $result->country ?></span>
-						</li>
-						<li>
-							<span class="dataTitle">Langue</span>
-							<span class="dataContent"><?php echo $result->language; ?></span>
-						</li>
-						<li>
-							<span class="dataPlot">Description</span>
-							<span class="dataContent"><?php echo $result->plot; ?></span>
-						</ul>
-					</div>
-				</div>
-				<?php
-
-			}
-
-			$movieDetail->closeCursor();
-		?> 
-		<?php
-		include("php/includes/scripts.php");
-		?>
-
-	</body>
-	</html>
+    </body>
+</html>
