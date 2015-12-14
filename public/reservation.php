0a4ea1376b334e40a8e3062fd7ff8ad565e135c2
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 17:43:01 +0000

    oui

--- public/reservation.php
+++ public/reservation.php
@@ -1,3 +1,35 @@
 <?php
-echo "lera";
+// Titre de la page
+$headTitle = "Cinewax - Programmation";
+
+// CSS de cette page
+$addCSS =  array("");
+
+// JS de cette page
+$addJS =  array("");
+
+// Queries de cette page
+$addPHP = array("");
+include("php/includes/head.php");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
