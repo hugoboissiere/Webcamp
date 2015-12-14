042560fe3021e555abdfa66c7b02bacdad2d677b
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 08:12:42 +0000

    test modif head

--- Defi Code Camps - Technologies du Web/ammani_i/admin/movies.php
+++ Defi Code Camps - Technologies du Web/ammani_i/admin/movies.php
@@ -116,19 +116,23 @@ include("php/includes/navigator.php");
                 </tr>
             </thead>
             <tbody>
-                <?php
-                // $countriesCompleteList = $dbh->query("SELECT * FROM cw_medias_countries  WHERE archive = 'false'");
-                // $countriesCompleteList->setFetchMode(PDO::FETCH_OBJ);
-                while ($result = $moviesCompleteList->fetch()) {
-                    
-                   echo "<tr data-id='Tableau_films'>
-                    <td class='title'>" . $result->title . "</td>
-                    <td>" . $result->genre . "</td>
-                </tr>";
-                }
-                // $countriesCompleteList->closeCursor();
+            <?php
+            while ($result = $moviesCompleteList->fetch()) {
                 ?>
-                
+                <tr data-id="<?php echo $result->id; ?>">
+                    <td class="title"><?php echo $result->title; ?></td>
+                    <td><?php echo $result->genre; ?></td>
+                    <td><?php echo $result->releaseDate; ?></td>
+                    <td><?php echo $result->runningTime; ?> min</td> 
+                    <td>
+                        <a href="#picturePoster" class='inline inactiveLink picturePoster' ><img src="resources/imgs/layout/pictureGrey.png"/></a>
+                        <a href="#pictureCover" class='inline inactiveLink pictureCover' ><img src="resources/imgs/layout/pictureGrey.png"/></a>
+                    </td> 
+                </tr>
+                <?php
+            }
+            $moviesCompleteList->closeCursor();
+            ?>    
             </tbody>
         </table>
     </div>
