73d78894dd878cccf8c0734c337cbd9172940ecb
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 17:40:36 +0000

    oui

--- Defi Code Camps - Technologies du Web/ammani_i/public/programmation.php
+++ Defi Code Camps - Technologies du Web/ammani_i/public/programmation.php
@@ -52,7 +52,7 @@ include("php/includes/head.php");
             <p style="font-size: 14px;color:rgb(51, 51, 51);">De <?php echo $result->realisator; ?></p>
             <p style="font-size: 14px;color:rgb(51, 51, 51);"><?php echo $result->runningTime; ?> min (<?php echo $runningTimeHour; ?>)</p>
             <p style="color:#68D2C3;"><?php echo strftime('%Y-%m-%d %H:%M:%S', date('Y-m-d H:i:s',strtotime($date)));?></p>            
-            <?php echo '<a href="reservation.php?id=' . $var . '">' ?><p style="color:#68D2C3;">Reservation</p></a>  
+            <?php echo '<a href="reservation.php?id=' . $result->id . '">' ?><p style="color:#68D2C3;">Reservation</p></a>  
          </div>
     </li>
 
