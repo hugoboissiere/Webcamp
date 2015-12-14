433654aa8bf867e253a6a630e5f984ebae1d98c1
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 12:04:10 +0000

    ca marche pas

--- admin/home.php
+++ admin/home.php
@@ -15,8 +15,6 @@ $addPHP = array("");
 include("php/includes/queries/count.php");
 include("php/includes/navigator.php");
 ?>
-<?php
-	// echo $_SESSION['auth']['job'];
         <!-- main content here -->
     </main>
     <script src="js/layoutNavigator.js"></script> <!-- Resource jQuery -->
