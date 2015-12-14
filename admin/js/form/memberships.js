cc68038032cbfc1162d3e02278184069d9c7a9ce
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 16:03:06 +0000

    on enleve les alertes

--- admin/js/form/memberships.js
+++ admin/js/form/memberships.js
@@ -92,7 +92,7 @@ $().ready(function() {
     $("#modificationForm").validate({
         submitHandler: function() {
                 var formData = $("#modificationForm").serialize();
-                alert(formData);
+                // alert(formData);
                 $.ajax({
                     type: "POST",
                     url: "php/queries/update.php",
