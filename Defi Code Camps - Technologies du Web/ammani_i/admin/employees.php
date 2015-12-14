b781a1c976a82d31478d2c5d170972f6c3025778
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-14 15:08:12 +0000

    ca marche plus ou moins

--- Defi Code Camps - Technologies du Web/ammani_i/admin/employees.php
+++ Defi Code Camps - Technologies du Web/ammani_i/admin/employees.php
@@ -22,18 +22,23 @@ include("php/includes/navigator.php");
     <!--Titre-->
     <div id="titleAndFilter">
         <h2><?php echo $pageTitle; ?></h2>
-        <ul>
-            <li>
-                <a class='inline' href='#add'>Ajouter</a>
-            </li>
-            <li>
-                <a class='inline inactiveLink' href='#modify'>Modifier</a>
-            </li>
-            <li>
-                <a class='inline inactiveLink' href='#archive'>Archiver</a>
-            </li>
-        </ul>
-    </div>'
+        <?php
+        if($_SESSION['auth']['job'] < 7)
+        {
+         echo "<ul>
+         <li>
+         <a class='inline' href='#add'>Ajouter</a>
+         </li>
+         <li>
+         <a class='inline inactiveLink' href='#modify'>Modifier</a>
+         </li>
+         <li>
+         <a class='inline inactiveLink' href='#archive'>Archiver</a>
+         </li>
+         </ul>";
+     }
+     ?>
+ </div>
 
     <div style='display:none'>
 
