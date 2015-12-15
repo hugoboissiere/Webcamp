2dc276146221cc5e344788c69e0feff9fe109d9a
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 16:33:42 +0000

    test upload

--- public/upload.php
+++ public/upload.php
@@ -8,13 +8,13 @@ print_r($img_extensions);
 // //2. substr(chaine,1) ignore le premier caractère de chaine.
 // //3. strtolower met l'extension en minuscules.
 $extension_upload = strtolower(substr(strrchr($_FILES['profilpic']['name'], '.'), 1));
-// if (in_array($extension_upload,$img_extensions))
-// {
-// 	echo "Extension correcte";
-// else
-// {
-// 	echo "logie";
-// }
+if (in_array($extension_upload,$img_extensions))
+{
+	echo "Extension correcte";
+else
+{
+	echo "logie";
+}
 echo $extension_upload;
 echo "akakak";
 if($erreur)
