38b70c3edb969ed6a35231a5711ec746a64b32e3
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 09:53:20 +0000

    oui

--- public/reservation.php
+++ public/reservation.php
@@ -86,7 +86,7 @@ while ($result = $movieDetail->fetch()) {
 			<img src="<?php echo $img; ?>" alt="">
 		</div>
 		<div id="text">
-			<div id="title" style="width = 200px">
+			<div>
 				<ul>
 					<li>
 						<span class="dataTitle">Date de sortie</span>
