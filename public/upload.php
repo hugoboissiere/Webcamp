7c574c7adba17d65444552cc4736337c716ba70e
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 16:24:12 +0000

    test upload

--- public/upload.php
+++ public/upload.php
@@ -1,21 +1,21 @@
 <?php
-if ($_FILES['profilpic']['error'] > 0)
-	$erreur = "Erreur lors du transfert";
-if ($_FILES['profilpic']['size'] > $maxsize)
-	$erreur = "Le fichier est trop gros";
+// if ($_FILES['profilpic']['error'] > 0)
+// 	$erreur = "Erreur lors du transfert";
+// if ($_FILES['profilpic']['size'] > $maxsize)
+// 	$erreur = "Le fichier est trop gros";
 
-$img_extensions = array('jpg', 'jpeg', 'gif', 'png');
-//1. strrchr renvoie l'extension avec le point (« . »).
-//2. substr(chaine,1) ignore le premier caractère de chaine.
-//3. strtolower met l'extension en minuscules.
-$extension_upload = strtolower(substr(strrchr($_FILES['profilpic']['name'], '.'), 1));
-if (in_array($extension_upload,$img_extensions))
-{
-	echo "Extension correcte";
-else
-{
-	echo "logie";
-}
-echo $erreur;
+// $img_extensions = array('jpg', 'jpeg', 'gif', 'png');
+// //1. strrchr renvoie l'extension avec le point (« . »).
+// //2. substr(chaine,1) ignore le premier caractère de chaine.
+// //3. strtolower met l'extension en minuscules.
+// $extension_upload = strtolower(substr(strrchr($_FILES['profilpic']['name'], '.'), 1));
+// if (in_array($extension_upload,$img_extensions))
+// {
+// 	echo "Extension correcte";
+// else
+// {
+// 	echo "logie";
+// }
+echo "akakak";
 
 ?>
