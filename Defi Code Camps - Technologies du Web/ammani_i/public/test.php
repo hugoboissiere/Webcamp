46db9ae3ef40a710e0d4e53683a9145f9f109525
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 14:49:35 +0000

    test upload public

--- Defi Code Camps - Technologies du Web/ammani_i/public/test.php
+++ Defi Code Camps - Technologies du Web/ammani_i/public/test.php
@@ -13,16 +13,10 @@ $addPHP = array("");
 include("php/includes/head.php");
 ?>
 
-<form method="post" action="reception.php" enctype="multipart/form-data">
-     <label for="icone">Icône du fichier (JPG, PNG ou GIF | max. 15 Ko) :</label><br />
-     <input type="file" name="icone" id="icone" /><br />
+<form method="post" action="upload.php" enctype="multipart/form-data">
      <label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
      <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
      <input type="file" name="mon_fichier" id="mon_fichier" /><br />
-     <label for="titre">Titre du fichier (max. 50 caractères) :</label><br />
-     <input type="text" name="titre" value="Titre du fichier" id="titre" /><br />
-     <label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
-     <textarea name="description" id="description"></textarea><br />
      <input type="submit" name="submit" value="Envoyer" />
 </form> 
 
