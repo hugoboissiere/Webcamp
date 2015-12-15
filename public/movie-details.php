4cf76e1800f67051c54cb4e0da22556e5973fba7
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 10:30:55 +0000

    oui

--- public/movie-details.php
+++ public/movie-details.php
@@ -15,108 +15,108 @@ $movieDetail = $dbh->query("SELECT * FROM cw_medias_movies WHERE id = '" . $id .
 $movieDetail->setFetchMode(PDO::FETCH_OBJ);
 
 
- while ($result = $movieDetail->fetch()) {
+while ($result = $movieDetail->fetch()) {
 
 
-     
-     
-     
-       $filenamePoster = 'resources/imgs/content/movies/'.$result->id.'Poster.jpg';
+ 
+ 
+ 
+ $filenamePoster = 'resources/imgs/content/movies/'.$result->id.'Poster.jpg';
 
-if (file_exists($filenamePoster)) {
-    $img = $filenamePoster;
+ if (file_exists($filenamePoster)) {
+  $img = $filenamePoster;
 } else {
-   $rand =  rand(1,2);
-   $img = 'resources/imgs/content/movies/defaultPoster'.$rand.'.jpg';
+ $rand =  rand(1,2);
+ $img = 'resources/imgs/content/movies/defaultPoster'.$rand.'.jpg';
 }  
 
 
-  $filenameCover = 'resources/imgs/content/movies/'.$result->id.'Cover.jpg';
+$filenameCover = 'resources/imgs/content/movies/'.$result->id.'Cover.jpg';
 
 if (file_exists($filenameCover)) {
-    $cover = $filenameCover;
+  $cover = $filenameCover;
 } else {
-   $rand =  rand(1,2);
-   $cover = 'resources/imgs/content/movies/defaultCover'.$rand.'.jpg';
+ $rand =  rand(1,2);
+ $cover = 'resources/imgs/content/movies/defaultCover'.$rand.'.jpg';
 }  
-     
+
 
 ?>
-
+<meta charset="UTF-8">
 <script>
-    $(document).ready(function(){
-        $(".trailer").colorbox({iframe:true, innerWidth:500, innerHeight:409});
+$(document).ready(function(){
+  $(".trailer").colorbox({iframe:true, innerWidth:500, innerHeight:409});
 
-    });
+});
 </script>
 <div id="cover">
-      <span></span>
-      <img src="<?php echo $cover; ?>" alt="">
+  <span></span>
+  <img src="<?php echo $cover; ?>" alt="">
 
-      <h2>
-        <?php echo $result->title; ?>
-        <span><?php echo $result->titleOriginal; ?></span>
+  <h2>
+    <?php echo $result->title; ?>
+    <span><?php echo $result->titleOriginal; ?></span>
 
-      </h2>
-      <?php if ($result->type != ""){ ?>
-      <a class="trailer" href="<?php echo $result->type; ?>">Bande-Annonce</a>
-      <?php } ?>
+  </h2>
+  <?php if ($result->type != ""){ ?>
+  <a class="trailer" href="<?php echo $result->type; ?>">Bande-Annonce</a>
+  <?php } ?>
+</div>
+<div id="informations" style="margin-top:300px;">
+  <div id="picture">
+    <img src="<?php echo $img; ?>" alt="">
   </div>
-  <div id="informations" style="margin-top:300px;">
-    <div id="picture">
-        <img src="<?php echo $img; ?>" alt="">
+  <div id="text">
+    <div id="title">
+      <ul>
+        <li>
+          <span class="dataTitle">Date de sortie</span>
+          <span class="dataContent"><?php echo $result->releaseDate; ?></span>
+        </li>
+        <li>
+          <span class="dataTitle">Durée</span>
+          <span class="dataContent"><?php echo $result->runningTime; ?> Minutes</span>
+        </li>
+        <li>
+          <span class="dataTitle">Genre</span>
+          <span class="dataContent"><?php echo $result->genre;?></span>
+        </li>
+        <li>
+          <span class="dataTitle">Réalisateur</span>
+          <span class="dataContent"><?php echo $result->realisator; ?></span>
+        </li>
+        <li>
+          <span class="dataTitle">Acteurs</span>
+          <span class="dataContent"><?php echo $result->actors; ?></span>
+        </li>
+        <li>
+          <span class="dataTitle">Production</span>
+          <span class="dataContent"><?php echo $result->production; ?></span>
+        </li>
+        <li>
+          <span class="dataTitle">Pays</span>
+          <span class="dataContent"><?php echo $result->country ?></span>
+        </li>
+        <li>
+          <span class="dataTitle">Langue</span>
+          <span class="dataContent"><?php echo $result->language; ?></span>
+        </li>
+        <li>
+          <span class="dataPlot">Description</span>
+          <span class="dataContent"><?php echo $result->plot; ?></span>
+        </ul>
+      </div>
     </div>
-    <div id="text">
-        <div id="title">
-            <ul>
-                <li>
-                    <span class="dataTitle">Date de sortie</span>
-                    <span class="dataContent"><?php echo $result->releaseDate; ?></span>
-                </li>
-                <li>
-                    <span class="dataTitle">Durée</span>
-                    <span class="dataContent"><?php echo $result->runningTime; ?> Minutes</span>
-                </li>
-                <li>
-                    <span class="dataTitle">Genre</span>
-                    <span class="dataContent"><?php echo $result->genre;?></span>
-                </li>
-                <li>
-                    <span class="dataTitle">Réalisateur</span>
-                    <span class="dataContent"><?php echo $result->realisator; ?></span>
-                </li>
-                <li>
-                    <span class="dataTitle">Acteurs</span>
-                    <span class="dataContent"><?php echo $result->actors; ?></span>
-                </li>
-                <li>
-                    <span class="dataTitle">Production</span>
-                    <span class="dataContent"><?php echo $result->production; ?></span>
-                </li>
-                <li>
-                    <span class="dataTitle">Pays</span>
-                    <span class="dataContent"><?php echo $result->country ?></span>
-                </li>
-                <li>
-                    <span class="dataTitle">Langue</span>
-                    <span class="dataContent"><?php echo $result->language; ?></span>
-                </li>
-                <li>
-                    <span class="dataPlot">Description</span>
-                    <span class="dataContent"><?php echo $result->plot; ?></span>
-            </ul>
-        </div>
-    </div>
-<?php
+    <?php
     
-            }
+  }
 
-            $movieDetail->closeCursor();
+  $movieDetail->closeCursor();
 
-            ?> 
-<?php
-include("php/includes/scripts.php");
-?>
+  ?> 
+  <?php
+  include("php/includes/scripts.php");
+  ?>
 
-    </body>
+</body>
 </html>
