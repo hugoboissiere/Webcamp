8be88c039c43106791cbc653da459de234cc4bbc
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 14:44:09 +0000

    test upload public

---
+++ public/test.php
@@ -0,0 +1,41 @@
+<?php
+// Titre de la page
+$headTitle = "Cinewax - Films disponibles";
+
+// CSS de cette page
+$addCSS =  array("");
+
+// JS de cette page
+$addJS =  array("");
+
+// Queries de cette page
+$addPHP = array("");
+include("php/includes/head.php");
+?>
+
+
+
+
+
+
+<?php
+include("php/includes/scripts.php");
+?>
+
+<!--scripts Js-->
+
+
+<form method="post" action="reception.php" enctype="multipart/form-data">
+     <label for="icone">Icône du fichier (JPG, PNG ou GIF | max. 15 Ko) :</label><br />
+     <input type="file" name="icone" id="icone" /><br />
+     <label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
+     <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+     <input type="file" name="mon_fichier" id="mon_fichier" /><br />
+     <label for="titre">Titre du fichier (max. 50 caractères) :</label><br />
+     <input type="text" name="titre" value="Titre du fichier" id="titre" /><br />
+     <label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
+     <textarea name="description" id="description"></textarea><br />
+     <input type="submit" name="submit" value="Envoyer" />
+</form>
+</body>
+</html>
