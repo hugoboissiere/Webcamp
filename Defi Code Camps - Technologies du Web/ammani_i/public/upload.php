d7ae772bd10d8fd53e7143b92a74ef8ecc72a33b
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 16:22:39 +0000

    test upload

--- Defi Code Camps - Technologies du Web/ammani_i/public/upload.php
+++ Defi Code Camps - Technologies du Web/ammani_i/public/upload.php
@@ -1,1 +1,22 @@
-<?php echo "salut"; ?>
+<?php
+echo "steak";
+if ($_FILES['profilpic']['error'] > 0)
+	$erreur = "Erreur lors du transfert";
+if ($_FILES['profilpic']['size'] > $maxsize)
+	$erreur = "Le fichier est trop gros";
+
+$img_extensions = array('jpg', 'jpeg', 'gif', 'png');
+//1. strrchr renvoie l'extension avec le point (« . »).
+//2. substr(chaine,1) ignore le premier caractère de chaine.
+//3. strtolower met l'extension en minuscules.
+$extension_upload = strtolower(substr(strrchr($_FILES['icone']['name'], '.'), 1));
+if (in_array($extension_upload,$img_extensions))
+{
+	echo "Extension correcte";
+else
+{
+	echo "logie";
+}
+echo $erreur;
+
+?>
