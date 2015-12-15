40463a7ae53bf4da21427142773a098681366dae
boissi_h <boissi_h@4c2244bb-a0f5-498a-8e7d-510303cd71f8>
2015-12-15 16:30:05 +0000

    test upload

--- Defi Code Camps - Technologies du Web/ammani_i/public/upload.php
+++ Defi Code Camps - Technologies du Web/ammani_i/public/upload.php
@@ -2,18 +2,18 @@
 if ($_FILES['profilpic']['error'] > 0)
 	$erreur = "Erreur lors du transfert";
 
-// $img_extensions = array('jpg', 'jpeg', 'gif', 'png');
-// //1. strrchr renvoie l'extension avec le point (« . »).
-// //2. substr(chaine,1) ignore le premier caractère de chaine.
-// //3. strtolower met l'extension en minuscules.
-// $extension_upload = strtolower(substr(strrchr($_FILES['profilpic']['name'], '.'), 1));
-// if (in_array($extension_upload,$img_extensions))
-// {
-// 	echo "Extension correcte";
-// else
-// {
-// 	echo "logie";
-// }
+$img_extensions = array('jpg', 'jpeg', 'gif', 'png');
+//1. strrchr renvoie l'extension avec le point (« . »).
+//2. substr(chaine,1) ignore le premier caractère de chaine.
+//3. strtolower met l'extension en minuscules.
+$extension_upload = strtolower(substr(strrchr($_FILES['profilpic']['name'], '.'), 1));
+if (in_array($extension_upload,$img_extensions))
+{
+	echo "Extension correcte";
+else
+{
+	echo "logie";
+}
 echo "akakak";
 if($erreur)
 	echo $erreur;
